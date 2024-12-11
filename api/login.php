a519a77f53033d29b56058cb202f3cc9226e843d
Joey, K <40598758+naeyanika@users.noreply.github.com>
2024-12-11 10:01:46 +0700

    Update login.php

--- api/login.php
+++ api/login.php
@@ -1,6 +1,5 @@
-<userStyle>Normal</userStyle>
-
 <?php
+ob_start(); // Start output buffering
 session_start();
 session_set_cookie_params(3600);
 
@@ -8,7 +7,7 @@ session_set_cookie_params(3600);
 if (isset($_SESSION['last_activity']) && (time() - $_SESSION['last_activity'] > 3600)) {
     // Sesi berakhir, logout pengguna
     session_destroy();
-    header('Location: login.html');
+    header('Location: /login.html');
     exit();
 }
 
@@ -21,22 +20,22 @@ $users = [
     'audit' => 'audit'
 ];
 
+$error = '';
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $username = isset($_POST['username']) ? $_POST['username'] : '';
     $password = isset($_POST['password']) ? $_POST['password'] : '';
-
+    
     if (isset($users[$username]) && $users[$username] === $password) {
-
         $_SESSION['username'] = $username;
         $_SESSION['logged_in'] = true;
-        header('Location: index.html'); 
+        header('Location: /index.html'); 
         exit();
     } else {
         $error = "Username atau password salah!";
     }
 }
 
-include 'login.html';
-
+include '../login.html'; // Adjust path to point to login.html outside of /api
+ob_end_flush();
 ?>
